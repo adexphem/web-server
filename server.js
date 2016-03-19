b0b8ff80226f483e4fc8a581926924a47bc55641
phemmy <adexphem4real@gmail.com>
2016-03-19 17:24:16 +0100

    refactoring done for middleware.js

--- server.js
+++ server.js
@@ -3,16 +3,7 @@ var app = express();
 var APP_PORT = 3000;
 var currentDate = new Date().toString();
 
-var middleware = {
-	requireAuthentication: function(req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function(req, res, next) {
-		console.log('Request ' + req.method + ' ' + req.originalUrl + ' made on ' + currentDate);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 
 
 app.use(middleware.logger);
